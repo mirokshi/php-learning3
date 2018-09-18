47207610381e19aa79cbbc22d3fc129482eaf582
mirokshi <rojasdiazmirokshi@gmail.com>
2018-09-18 20:51:49 +0200

    Chivatos PHP con echo

---
+++ Test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mirokshi
+ * Date: 18/09/18
+ * Time: 20:41
+ */
+
+
+class Test extends TestCase
+{
+     require("./hello.php");
+     $this -> ex
+}
---
+++ hello.php
@@ -0,0 +1,18 @@
+<?php
+
+//Variable
+$greeting='Hola mundo';
+
+
+//CHIVATOS (Ctrl+ / (teclado numerico))
+
+echo $greeting;
+//echo "Hola mundo"; //Hardcoded Toy example
+//var_dump('Hola mundo'); //muestra el texto con el tipo y la cantidad de caracteres
+
+
+//TEST -> TDD
+
+
+
+
