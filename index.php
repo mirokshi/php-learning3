ea43b1076148f91ed30cd39ee62b2c604975dac9
mirokshi <rojasdiazmirokshi@gmail.com>
2018-09-19 18:00:00 +0200

    HTTP GET para hacer las paginas dinamicas

--- index.php
+++ index.php
@@ -1,7 +1,42 @@
 <?php
 require 'function.php';
 
-$greeting= hello('mundo');
+var_dump($_GET);
+die();
+
+// ->GET es un vector
+//String -> $greeting='Hola';
+//Boolean -> $x true/false;
+//Enters -> $num = 15;
+
+////Array/Vector
+////
+////$frutas  = array();
+////$frutas = [];
+//
+//
+//$platano = 'platano';
+//$manzana = 'manzana';
+//$naranja = 'naranja';
+//$frutas = [$manzana , $platano ,$naranja];
+//
+//echo  $frutas[0];
+//die();
+
+
+////Array asociativo
+
+//$persona = 'Mirokshi';
+
+//$persona = [
+//    'name' => 'Mirokshi Rojas Diaz',
+//    'dni' => 'Y4786533'
+//    'mobile' => '616531219'
+//]
+
+$greeting= hello();
 
 
 require 'vistas/index.php';
+
+
